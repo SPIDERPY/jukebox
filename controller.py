f1127c5d324501249aa1057f4669d5f367aa8711
AB <ab@macbook-pro-de-ab.home>
2022-03-03 10:40:30 +0100

    Ajout des imports de modules

--- controller.py
+++ controller.py
@@ -1,3 +1,6 @@
+from vue import Speakers, Touchscreen
+from vue import CoinBox
+from card import PRICE_PER_SONG, Library
 
 
 class Controller:
@@ -16,10 +19,10 @@ class Controller:
     def play_next_song(self):
         """Joue le prochain son."""
         songs_to_suggest = []
-        for son in self.library:
+        for song in self.library:
             # filter logic
             songs_to_suggest.append(song)
             
         chosen_song = self.ui.prompt_for_next_song(songs_to_suggest)
-        request_money(PRICE_PER_SONG)
+        CoinBox.request_money(PRICE_PER_SONG)
         self.audio_output.play_song(chosen_song)
